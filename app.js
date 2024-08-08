e93766d93c580f9f670bc6b89c7a89eadbff2709
Melissa <lynch.melissat@gmail.com>
2024-08-08 14:18:28 -0400

    set up express server

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
 import { db } from './database/index.js';
 import { seedDb } from './database/models/index.js';
+import express from "express";
+import cors from "cors";
+
+//create express server
+const app = express();
+
+//configure app to handle request data
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//ignore browser requests for favicon file
+app.get('/favicon.ico', (req, res) => res.status(204));
+
+//define a route
+app.get("/hello", (request, response) => {
+  response.send("Hello, world!")
+});
+
+// Handle page not found:
+// gets triggered when a request is made to
+// an undefined route 
+app.use((req, res, next) => {
+  const error = new Error("Not Found, Please Check URL!");
+  error.status = 404;
+  next(error);
+});
+
+// Error-handling middleware: 
+// all express errors get passed to this
+// when next(error) is called 
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.log(req.originalUrl);
+  res.status(err.status || 500).send(err.message || "Internal server error.");
+});
 
 const syncDatabase = async () => {
     try {
       //the {force: true} option will clear the database tables
       //every time we restart the server
       //remove the option if you want the data to persist, ie: 
-      //await db.sync();
+        //await db.sync();
   
       await db.sync({force: true});
       console.log('------Synced to db------')
@@ -17,8 +53,7 @@ const syncDatabase = async () => {
     }  
 }
 
-const bootApp = async () => {
-    await syncDatabase();
-}
-
-bootApp();
+const PORT = 5001;
+syncDatabase().then(() => {
+  app.listen(PORT, console.log(`Server started on ${PORT}`));
+});
