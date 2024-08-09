82306c5874f767b8fc46f1cebc300f11691e35fd
Melissa <lynch.melissat@gmail.com>
2024-08-09 15:37:45 -0400

    finished task routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -4,7 +4,7 @@ import { Employee, Task } from '../database/models/index.js';
 //initialize router
 const router = express.Router();
 
-//define routes
+//define endpoints and route handlers
 
 /***** GET ALL TASKS: *****/
 router.get('/', async (req, res, next) => {
@@ -18,15 +18,31 @@ router.get('/', async (req, res, next) => {
 
 /***** DELETE TASK: *****/
 router.delete("/:id", function (req, res, next) {
-  Task.destroy({
-    where: {
-      id: req.params.id,
-    },
-  })
+  Task.destroy({ where: { id: req.params.id } })
     .then(() => res.status(200).json("Deleted a task!"))
     .catch((err) => next(err));
+});
+
+/***** ADD TASK: *****/
+router.post("/", function (req, res, next) {
+  Task.create(req.body)
+    .then((newTask) => res.status(200).json(newTask))
+    .catch((err) => next(err));
+});
+
+/***** EDIT TASK: *****/
+router.put("/:id", async (req, res, next) => {
+  try {
+    await Task.update(req.body, { where: { id: req.params.id } });
+    let updatedTask = await Task.findByPk(req.params.id);
+    res.status(200).json(updatedTask);
+  } catch (err) {
+    next(err);
+  }
 });
 
 export {
     router as taskRouter,
 };
+
+
